4ae059ec4a0e3e7de68991aeaa7fab0fd317dd1b
Mark Connelly <38826922-markeconnelly@users.noreply.replit.com>
2025-02-15 02:38:43 +0000

    Restored to 'e177c63e57c4e9f967bb862fcd714c6b5a585ab4'
    Replit-Restored-To:e177c63e57c4e9f967bb862fcd714c6b5a585ab4

--- client/src/components/ui/YouTubeBackground.tsx
+++ client/src/components/ui/YouTubeBackground.tsx
@@ -11,11 +11,11 @@ export function YouTubeBackground({
 }: YouTubeBackgroundProps) {
   return (
     <div className={cn("absolute inset-0 -z-10 overflow-hidden", className)}>
-      <iframe
-        id="ytplayer"
+      <iframe 
         className="absolute top-1/2 left-1/2 w-[100vw] h-[56.25vw] min-h-screen min-w-[177.77vh] -translate-x-1/2 -translate-y-1/2"
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&disablekb=1&fs=0&loop=1&playlist=${videoId}&iv_load_policy=3&mute=1`}
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0`}
         frameBorder="0"
+        allow="autoplay; encrypted-media"
         allowFullScreen
       />
       <div className="absolute inset-0 bg-black/40" />
