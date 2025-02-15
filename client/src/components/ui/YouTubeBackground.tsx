32c8efada2f0c7fb6b8cbd1c248737d7a4ee0e75
Mark Connelly <38826922-markeconnelly@users.noreply.replit.com>
2025-02-15 02:22:49 +0000

    Assistant checkpoint: Update YouTube iframe configuration
    Assistant generated file changes:
    - client/src/components/ui/YouTubeBackground.tsx: Update YouTube iframe configuration
    
    ---
    
    User prompt:
    
    can we use this instead?
    
    <iframe
        width="100%"
        height="100%"
        src="https://www.youtube.com/embed/OdObDXBzNYk?autoplay=1&loop=1&playlist=OdObDXBzNYk&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0"
        frameborder="0"
        allow="autoplay; encrypted-media"
        allowfullscreen>
    </iframe>

--- client/src/components/ui/YouTubeBackground.tsx
+++ client/src/components/ui/YouTubeBackground.tsx
@@ -12,12 +12,11 @@ export function YouTubeBackground({
 }: YouTubeBackgroundProps) {
   return (
     <div className={cn("absolute inset-0 -z-10 overflow-hidden", className)}>
-      <iframe
+      <iframe 
         className="absolute top-1/2 left-1/2 w-[100vw] h-[56.25vw] min-h-screen min-w-[177.77vh] -translate-x-1/2 -translate-y-1/2"
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${videoId}&playsinline=1&modestbranding=1&showinfo=0&rel=0&enablejsapi=1&origin=${window.location.origin}&version=3&iv_load_policy=3&fs=0&disablekb=1&end=${0}&playlist=${videoId}`}
-        title="Background Video"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0`}
         frameBorder="0"
+        allow="autoplay; encrypted-media"
         allowFullScreen
       />
       <div className="absolute inset-0 bg-black/40" />
